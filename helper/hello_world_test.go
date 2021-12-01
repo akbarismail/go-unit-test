92efb04962e8a2063fe05568cf0b10acad440d70
Akbar Ismail <ismailakbar.me@gmail.com>
2021-12-02 06:33:13 +0700

    menambahkan unit test di function hello-world

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -1,19 +1,41 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
 
-func TestHelloWorld(t *testing.T) {
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestHelloWorldRequire(t *testing.T) {
+	result := HelloWorld("Dan")
+	require.Equal(t, "Hello Dan", result, "Result must be 'Hello Dan'")
+	fmt.Println("TestHelloWorld with require done")
+}
+
+func TestHelloWorldAssert(t *testing.T) {
+	result := HelloWorld("Dan")
+	assert.Equal(t, "Hello Dan", result, "Result must be 'Hello Dan'")
+	fmt.Println("TestHelloWorld with assert done")
+}
+
+func TestHelloWorldJamey(t *testing.T) {
 	result := HelloWorld("Jamey")
 	if result != "Hello Jamey" {
 		// error
-		panic("result is not 'Hello Jamey'")
+		t.Error("Result must be 'Hello Jamey'")
 	}
+
+	fmt.Println("TestHelloWorldJamey done")
 }
 
 func TestHelloWorldAnjali(t *testing.T) {
 	result := HelloWorld("Anjali")
 	if result != "Hello Anjali" {
 		// error
-		panic("result is not 'Hello Anjali'")
+		t.Fatal("Result must be 'Hello Anjali'")
 	}
+
+	fmt.Println("TestHelloWorldAnjali done")
 }
